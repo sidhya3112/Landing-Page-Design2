fb94c0297c57fcb206443c1a2471c820e32c131d
Sidhya Jain <sidhyajain@gmail.com>
2022-07-01 22:40:11 +0530

    Added Course Box Component

---
+++ src/components/CourseInfo/CourseBox.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import { Rating } from 'react-simple-star-rating';
+import bookmark from '../../images/bookmark.png';
+import './CourseBox.css'
+
+const CourseBox = (props) => {
+    
+  return (
+    <div>
+        <ul className='course-box'>
+            <li><img className='course-img' src={props.img} alt="" /></li>
+            <li>
+                <ul className='course-title-bookmark'>
+                    <li className='course-title'>{props.title}</li>
+                    <li><img className='course-bookmark' src={bookmark} alt="" /></li>
+                </ul>
+            </li>
+            <li className='course-author'>{props.author}</li>
+            <li>
+                <ul className='course-star-rating'>
+                    <li className='rating-number'>{props.rating}</li>
+
+                    <li> <Rating  ratingValue={useState(props.rating)}
+                     fillColor= '#5F72BE' 
+                     emptyColor='white'
+                     size={20}
+                     allowHalfIcon={true}
+                     /> 
+                    </li>
+                </ul>
+            </li>
+            <li style={{paddingLeft:'27px', paddingTop:'12px'}}>
+                <button className='enroll-button-2 button-details'>
+                    Enroll Now
+                </button>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default CourseBox
--- src/components/CourseInfo/CourseInfo.js
+++ src/components/CourseInfo/CourseInfo.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './CourseInfo.css'
+import CourseBox from './CourseBox';
+import './CourseInfo.css';
+import courseImg from '../../images/course-img.png';
+import arrow from '../../images/arrow.png'
 
 const CourseInfo = () => {
   return (
@@ -7,6 +10,47 @@ const CourseInfo = () => {
         <ul className='course-info'>
             <li className='c-head c-text'>A broad range of courses</li>
             <li className='c-des c-text'>Choose from 100+ online video courses with new additions published every month</li>
+        </ul>
+        <ul className='course-boxes'>
+          <li>
+            <CourseBox
+            img={courseImg}
+            title='Learn SCRUM: The Complete Beginner Course'
+            author='Samay Jain, Ex-KPMG'
+            rating='4.5'
+            />
+          </li>
+
+          <li>
+            <CourseBox
+            img={courseImg}
+            title='Learn SCRUM: The Complete Beginner Course'
+            author='Samay Jain, Ex-KPMG'
+            rating='4.5'
+            />
+          </li>
+
+          <li>
+            <CourseBox
+            img={courseImg}
+            title='Learn SCRUM: The Complete Beginner Course'
+            author='Samay Jain, Ex-KPMG'
+            rating='4.5'
+            />
+          </li>
+
+          <li>
+            <CourseBox
+            img={courseImg}
+            title='Learn SCRUM: The Complete Beginner Course'
+            author='Samay Jain, Ex-KPMG'
+            rating='4.5'
+            />
+          </li>
+
+          <li>
+            <img className='arrow' src={arrow} alt="" />
+          </li>
         </ul>
     </div>
   )
