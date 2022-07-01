2649d52011da5894b0ab3f68f0f04999c3918e8b
Sidhya Jain <sidhyajain@gmail.com>
2022-07-02 02:21:34 +0530

    Made Changes in Testimonial Card

--- src/components/Testimonials/Card.js
+++ src/components/Testimonials/Card.js
@@ -4,7 +4,12 @@ import './Card.css'
 const Card = (props) => {
   return (
     <div className='card'>
-        <img className='card-img' src={props.img} alt="" />
+        <div className='card-pics'>
+            <img className='person-left person' src={props.left} alt="" />
+            <img className='card-img' src={props.img} alt="" />
+            <img className='person-right person' src={props.right} alt="" />
+        </div>
+        
         <ul className='card-body'>
             <li className='card-des'>"{props.des}"</li>
             <li className='card-name'>{props.name}</li>
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Testimonials.css';
 import arrow from '../../images/arrow.png'
 import Card from './Card';
-import person from '../../images/person1.png'
+import person1 from '../../images/person1.png'
+import person2 from '../../images/person2.png'
+import person3 from '../../images/person3.png'
 
 const Testimonials = () => {
   return (
@@ -15,7 +17,9 @@ const Testimonials = () => {
             <li><img className='back-arrow arrows' src={arrow} alt="" /></li>
             <li>
                 <Card
-                img={person}
+                left={person3}
+                right={person2}
+                img={person1}
                 des= 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil libero a assumenda ducimus dolores voluptate placeat in iure corporis facilis! Facere tempore accusamus quos quasi expedita corrupti, recusandae at provident.'
                 name='Md. Danish Ansari'
                 post='Learner, Govt. Employee @UPPCL'
